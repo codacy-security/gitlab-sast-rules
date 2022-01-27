72ab2e6d34089091c16d4cb0434d4a5fe90c250a
Isaac Dawson <idawson@gitlab.com>
2022-01-28 08:14:14 +0900

    add conditions to b101

---
+++ python/assert/test-assert_used_pytest.py
@@ -0,0 +1,12 @@
+import pytest 
+
+
+def f():
+    return 3
+
+
+def test_function():
+    assert 1 == 1
+
+def test_function2():
+    assert("test")
---
+++ python/assert/test-assert_used_unittest.py
@@ -0,0 +1,9 @@
+import unittest 
+
+
+def f():
+    return 3
+
+
+def test_function():
+    assert 1 == 1
