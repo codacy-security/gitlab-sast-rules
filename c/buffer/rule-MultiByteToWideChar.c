b363ff731c2f802759b3e287679228c6f4d9bc41
Michael Henriksen <mhenriksen@gitlab.com>
2023-11-09 09:22:08 +0100

    Make example code closer to compilable

--- c/buffer/rule-MultiByteToWideChar.c
+++ c/buffer/rule-MultiByteToWideChar.c
@@ -2,13 +2,11 @@
 #include<stdio.h>
 
 int main() {
-  printf("Hello HUMANS!");
-}
-
-void demo2() {
   char d[20];
   char s[20];
   int n;
+  char *szName = "SomeName";
+  wchar_t wszUserName[5];
 
   // ruleid: c_buffer_rule-MultiByteToWideChar
   MultiByteToWideChar(CP_ACP, 0, szName, -1, wszUserName, sizeof(wszUserName));
