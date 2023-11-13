57b4c317d93f22739b259ac486c7b9ca7b24439d
Michael Henriksen <mhenriksen@gitlab.com>
2023-11-13 13:22:15 +0100

    CI: Improve availability.rb check
    Improves ci/availability.rb to check whether a test case file with
    expected name and extension exists for each rule.
    
    To make the current rules pass, it was necessary to create placeholder
    test case files for rules that didn't have one. These will be filled
    out with actual example code as unit tests are implemented.
    
    The check is also prepared to check if test case files are empty, which
    is turned off for the time being, but when all files contain code, it
    should be enabled.

--- ci/availability.rb
+++ ci/availability.rb
@@ -4,22 +4,76 @@ module FileCheck
   def self.run
     ok = true
     Dir.entries('./').select { |f| File.directory?(f) }.each do |lang|
-      next if ['.git', '..', '.', 'ci'].include?(lang)
+      next if ['.git', '..', '.', 'ci', 'dist', 'docs', 'mappings'].include?(lang)
+
+      exts = self.extensions_for_lang(lang)
 
       Dir.glob("#{lang}/**/rule-*.yml").each do |file|
         dirname = File.dirname(file)
+        basename = File.basename(file, ".yml").delete_prefix("rule-")
 
-        if Dir.glob("#{dirname}/test-*.*").count.zero?
-          puts("no test cases in #{dirname}: ✘")
-          ok = false
+        test_files = []
+
+        exts.each do |ext|
+          test_files.push("#{dirname}/test-#{basename}.#{ext}")
+          test_files.push("#{dirname}/rule-#{basename}.#{ext}")
         end
+
+        test_file_found = ""
+
+        test_files.each do |test_file|
+          if File.file?(test_file)
+            # puts("test case file #{test_file} exists: ✔")
+            test_file_found = test_file
+
+            break
+          end
+        end
+
+        if test_file_found.empty?
+          puts("no test case file for #{file}: ✘")
+          ok = false
+
+          next
+        end
+
+        # TODO: When all test case files contain example code, this check should
+        # be activated to check for empty test case files.
+        # if self.empty_test_file?(test_file_found)
+        #   puts("empty test case file #{test_file_found}: ✘")
+        #   ok = false
+        # end
       end
     end
     ok
   end
+
+  def self.extensions_for_lang(lang)
+    case lang
+    when 'c'
+      return ['c', 'cpp']
+    when 'csharp'
+      return ['cs']
+    when 'javascript'
+      return ['js', 'html']
+    when 'python'
+      return ['py']
+    else
+      return [lang]
+    end
+  end
+
+  def self.empty_test_file?(filename)
+    File.open(filename, "r") do |f|
+      code_lines = f.readlines.delete_if { |l| l.strip!; l == "" || l.start_with?("//") }
+
+      return code_lines.empty?
+    end
+  end
 end
 
 if FileCheck.run
+  puts("test case files exist for all rules: ✔")
   exit(0)
 else
   exit(-1)
