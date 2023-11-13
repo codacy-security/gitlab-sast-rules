b1bc3ee172eeda4b4d200023df945691a10d3cda
Michael Henriksen <mhenriksen@gitlab.com>
2023-11-13 10:29:24 +0100

    Reintroduce pattern for _getts

--- c/buffer/rule-gets--getts.c
+++ c/buffer/rule-gets--getts.c
@@ -1,11 +1,16 @@
 // License: MIT (c) GitLab Inc.
 #include <stdio.h>
+#include <wchar.h>
 
 int main() {
   char string[50];
   printf("Please enter your str input");
   // ruleid: c_buffer_rule-gets-getts
   gets(string);
+  printf("Your input: %s", string);
+
+  // ruleid: c_buffer_rule-gets-getts
+  _getts(string);
   printf("Your input: %s", string);
 
   // ruleid: c_buffer_rule-gets-getts
